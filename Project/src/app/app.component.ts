d76f75fdeed142b0fe03c134500955958fff7afe
LindaMangena <Lindajazz42@gmail.com>
2021-05-23 15:22:17 -0400

    Template_Reference_variables_complete

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project';
+  title = 'Template_Reference_variables';
+
+
+constructor(){}
+
+ngOnInit(){}
+
+logMessage(value){
+  console.log(value);
+}
+
+
 }
